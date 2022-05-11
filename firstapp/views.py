1d8246f418c9c4b3ab203ced486ef53ab07df296
suminc <c961129@gmail.com>
2022-05-11 18:21:17 +0900

    django

--- firstapp/views.py
+++ firstapp/views.py
@@ -5,11 +5,15 @@ from .models import Curriculum
 
 def show(request):
     curriculum = Curriculum.objects.all()
-    result = ''
-    for c in curriculum:
-        result += c.name + '<br>'
-    return HttpResponse(result)
-    
+    # result = ''
+    # for c in curriculum:
+    #     result += c.name + '<br>'
+    # return HttpResponse(result)
+    return render(
+        request, 'show.html', 
+        {'data' : curriculum}
+        )
+
 def insert(request):
     # 1-linux 입력
     Curriculum.objects.create(name='linux')
